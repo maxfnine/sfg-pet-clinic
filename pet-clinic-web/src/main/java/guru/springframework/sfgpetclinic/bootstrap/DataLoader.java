9d4537cdba4d74bf0efe9c47dfcf1474bf146974
maxfnine <maxfnine@gmail.com>
2020-11-06 20:59:20 +0200

    Adding lombok improvements for code clarity

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -71,6 +71,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("3123123131312");
 
+
+
         Pet mikesPet= new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
