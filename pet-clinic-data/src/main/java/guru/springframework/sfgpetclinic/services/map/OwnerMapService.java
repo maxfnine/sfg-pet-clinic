0bf1b05710548fa2e1ba54c25dce8dddb5ad8ed0
maxfnine <maxfnine@gmail.com>
2020-11-06 18:32:24 +0200

    Added profile SDJpa and map services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -2,14 +2,15 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
-import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Profile({"default","map"})
 @Service
 public class OwnerMapService extends AbstractMapService<Owner,Long> implements OwnerService {
     private final PetTypeService petTypeService;
