10d389781b1eb56d7219d0737807fb5162d3ab87
maxfnine <maxfnine@gmail.com>
2020-11-08 17:13:32 +0200

    Adding JUnit 5 Tests

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -1,7 +1,13 @@
 package guru.springframework.sfgpetclinic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-class SfgPetClinicApplicationTest {
-
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+    @Test
+    public void contextLoads(){}
 }
