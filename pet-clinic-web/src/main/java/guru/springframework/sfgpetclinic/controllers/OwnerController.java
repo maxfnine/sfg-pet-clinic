52a60cac8d0552203b9910ffd76332747e23394c
maxfnine <maxfnine@gmail.com>
2020-11-13 21:34:32 +0200

    Added find owner by last name

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,12 +1,16 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/owners")
@@ -17,16 +21,30 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/index","/index.html"})
-    public String listOwners(Model model){
-        model.addAttribute("owners",ownerService.findAll());
-        return "owners/index";
+    @RequestMapping("/find")
+    public  String findOwners(Model model){
+        model.addAttribute("owner", Owner.builder().build());
+        return "/owners/findOwners";
     }
 
-    @RequestMapping("/find")
-    public  String findOwners(){
-        return "notimplemented";
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result,Model model){
+        if(owner.getLastName()==null){
+            owner.setLastName("");
+        }
+        List<Owner> owners = ownerService.findAllByLastNameLike(owner.getLastName());
+        if(owners.isEmpty()){
+            result.rejectValue("lastName","notFound","not found");
+            return "owners/findOwners";
+        }
+        else if(owners.size()==1){
+            return "redirect:/owners/"+owners.get(0).getId();
+        }else{
+            model.addAttribute("selections",owners);
+            return "owners/ownersList";
+        }
     }
+
 
     @GetMapping("/{ownerId}")
     public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
